d8cebce229ea73312bff6dd99279ba3f58d383d0
Cactiw <Knyazev1514@gmail.com>
2022-03-17 01:38:47 +0300

    Fixed model

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,79 @@
 <template>
+  <n-space vertical align="center">
+    <n-grid :cols="12" style="width: 80%">
+      <n-grid-item :span="12">
+        <img src="ngram.png" alt="logo"/>
+      </n-grid-item>
+
+      <n-grid-item :span="12">
+        <div class="items-center">
+          <n-auto-complete
+              placeholder="Введите запрос"
+              :options="autoCompleteOptions"
+              :loading="autoCompleteLoading"
+              :on-update:value="handleAutocompleteUpdate"
+              :render-label="renderLabel"
+          />
+        </div>
+      </n-grid-item>
+
+    </n-grid>
+
+  </n-space>
   <n-button @click="modelSetupRef.showModal()">Модель</n-button>
 
   <ModelSetupModalComponent ref="modelSetupRef"></ModelSetupModalComponent>
 </template>
 
 <script>
-import {NCard, NH5, NText, NUpload, NSpace, NButton, NInput} from "naive-ui";
+import {NCard, NH5, NText, NUpload, NSpace, NButton, NInput, NGrid, NGridItem, NAutoComplete, NTag} from "naive-ui";
 import ModelSetupModalComponent from "@/components/ModelSetupModalComponent";
-import {ref} from "vue";
+import {h, inject, ref} from "vue";
+import axios from "axios";
 
 export default {
   name: 'HelloWorld',
   components: {
     ModelSetupModalComponent,
-    NCard, NH5, NText, NUpload, NSpace, NButton, NInput
+    NCard, NH5, NText, NUpload, NSpace, NButton, NInput, NGrid, NGridItem, NAutoComplete
   },
   setup() {
+    const defaultURL = inject('defaultURL')
+
     const modelSetupRef = ref(null)
 
+    const autoCompleteOptions = ref([])
+    const autoCompleteLoading = ref(false)
+
+    async function handleAutocompleteUpdate(text) {
+      if (!text) {
+        autoCompleteOptions.value = []
+        return
+      }
+      autoCompleteLoading.value = true
+
+      try {
+        const res = await axios.get(`${defaultURL}/api/getAutocomplete`, {params: {
+            start_string: text
+          }})
+        autoCompleteOptions.value = res.data
+        console.log(res.data)
+      } finally {
+        autoCompleteLoading.value = false
+      }
+    }
+
+    function renderLabel(option) {
+      return [
+        option[0].join(' '),
+        " ",
+        h(NTag, { size: "small", type: "info" }, { default: () => option[1].toString().substr(0, 6) })
+      ]
+
+    }
+
     return {
-      modelSetupRef
+      modelSetupRef, autoCompleteOptions, autoCompleteLoading, handleAutocompleteUpdate, renderLabel
     }
   }
 }
